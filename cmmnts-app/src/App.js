78b0fb08b047b7fdcbe04164d34389390b944c38
Sai Balaram <Kapavarapu.SaiBalaram@philips.com>
2021-06-10 01:51:56 +0530

    cmmnt list is added to app.js

--- cmmnts-app/src/App.js
+++ cmmnts-app/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
-import CommentCard from "./components/CommentCard";
+
+import CommentList from "./components/CommentList";
 
 function App() {
-  return <CommentCard comment="good book" name="balaram" />;
+  const comment1 = { comment: "good book", name: "balaram" };
+  const comment2 = { comment: "bad book", name: "sai balaram" };
+  const comments = [comment1, comment2];
+  return <CommentList comments={comments} />;
 }
 
 export default App;
